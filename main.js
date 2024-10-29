9c24ed223add042fb8118e6a3c8303397898f4d2
unknown <betim009@gmail.com>
2024-10-29 14:30:58 -0300

    createProdutos

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ import { produtos } from "./data/data.js";
 
 
 // Setores
-
-
+const divResultados = document.getElementById('div-resultados');
+const divCarrinho = document.getElementById('div-carrinho');
 
 
 // Variaveis globais
@@ -14,6 +14,22 @@ import { produtos } from "./data/data.js";
 
 
 // Funções
+// Criar uma função para criar os primeiros elementos na tela.
+function createProdutos(listaDeProdutos) {
+    listaDeProdutos.map((e, i) => (
+        divResultados.innerHTML += `
+            <div>
+                <img class="img-thumb" src="${e.thumb}" alt="" />
+                <h2>${e.nome}</h2>
+                <p>${e.preco}</p>
+                <button>Add Carrinho</button>
+                <button>Finalizar</button>
+            </div> 
+        `
+    ));
+};
+
+createProdutos(produtos);
 
 
 
